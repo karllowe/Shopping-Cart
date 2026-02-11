8ec634132697740403e5ae76626e6a0cdbc9aea7
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-11 15:57:09 +0000

    added dummy shopping info api call

--- src/routes/Browse.jsx
+++ src/routes/Browse.jsx
@@ -1,9 +1,30 @@
-import classes from "./browse.module.css"
+import {useEffect, useState} from "react";
+import classes from "./browse.module.css";
 
 function Browse() {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const apiReturn = await fetch("https://fakestoreapi.com/products");
+                const res = await apiReturn.json();
+                setItems(res);
+                setLoading(false);
+            } catch(error) {
+                setError(error)
+            }
+        })();
+    }, [])
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>A network error was encountered</p>
+
     return (
         <main className={classes.main}>
-            <h2 className>Let's shop!</h2>
+            <h2 >Let's shop!</h2>
         </main>
     )
 }
