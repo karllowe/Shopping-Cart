de9d1fbd4841ec53bd914ec7494f8087897eaa7e
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-12 10:43:08 +0000

    Added basic styling and add button to browse cards

--- src/components/shoppingCard/ShoppingCard.jsx
+++ src/components/shoppingCard/ShoppingCard.jsx
@@ -1,10 +1,17 @@
-import classes from "./shoppingCard.module.css"
+import classes from "./shoppingCard.module.css";
+import { TextButton } from "../textButton/TextButton";
 
 function ShoppingCard({item}) {
     return(
         <div className={classes.body}>
             <img src={item.image} alt={item.title} className={classes.img}/>
-            <h3>{item.title}</h3>
+            <div className={classes.details}>
+                <p className={classes.title}>{item.title}</p>
+                <div className={classes.bottomRow}>
+                    <p>£{item.price}</p>
+                    <TextButton text={"Add"} type={"primary"}/>
+                </div>
+            </div>
         </div>
     )
 }
